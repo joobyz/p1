278b140c664d6277c31d1b63ba647865ae348dea
Joobyz <sorci0988@gmail.com>
2018-07-20 13:04:25 +0200

    p1

---
+++ p1/mainScreen.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace p1
+{
+    partial class mainScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_left = new System.Windows.Forms.TextBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.txt_right = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_left
+            // 
+            this.txt_left.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txt_left.Location = new System.Drawing.Point(13, 13);
+            this.txt_left.Multiline = true;
+            this.txt_left.Name = "txt_left";
+            this.txt_left.ReadOnly = true;
+            this.txt_left.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_left.Size = new System.Drawing.Size(179, 201);
+            this.txt_left.TabIndex = 0;
+            // 
+            // btn_start
+            // 
+            this.btn_start.BackColor = System.Drawing.Color.ForestGreen;
+            this.btn_start.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_start.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_start.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_start.Location = new System.Drawing.Point(13, 221);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(179, 50);
+            this.btn_start.TabIndex = 2;
+            this.btn_start.Text = "START";
+            this.btn_start.UseVisualStyleBackColor = false;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.BackColor = System.Drawing.Color.Red;
+            this.btn_stop.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_stop.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_stop.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_stop.Location = new System.Drawing.Point(211, 221);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(179, 50);
+            this.btn_stop.TabIndex = 3;
+            this.btn_stop.Text = "STOP";
+            this.btn_stop.UseVisualStyleBackColor = false;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // btn_save
+            // 
+            this.btn_save.BackColor = System.Drawing.Color.Navy;
+            this.btn_save.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_save.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_save.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_save.Location = new System.Drawing.Point(13, 278);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(377, 41);
+            this.btn_save.TabIndex = 4;
+            this.btn_save.Text = "MENTÉS FÁJLBA";
+            this.btn_save.UseVisualStyleBackColor = false;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // txt_right
+            // 
+            this.txt_right.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txt_right.Location = new System.Drawing.Point(211, 14);
+            this.txt_right.Multiline = true;
+            this.txt_right.Name = "txt_right";
+            this.txt_right.ReadOnly = true;
+            this.txt_right.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_right.Size = new System.Drawing.Size(179, 201);
+            this.txt_right.TabIndex = 5;
+            // 
+            // mainScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 331);
+            this.Controls.Add(this.txt_right);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.txt_left);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "mainScreen";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainScreen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_left;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.TextBox txt_right;
+    }
+}
+
---
+++ p1/mainScreen.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GetID;
+
+namespace p1
+{
+    public partial class mainScreen : Form
+    {
+        private string      logname;
+        private string      val;
+        private int         matchesFirst;
+        private int         matchesSecond;
+
+        GetID.GetID getID = new GetID.GetID();
+        
+
+        public mainScreen()
+        {
+            InitializeComponent();
+
+            getID.ValueChanged += (s, e) => {
+                CheckValue(getID.Value);
+            };
+            getID.ErrorChanged += (s, e) => {
+                ShowError(getID.ErrorMessage);
+            };
+        }
+
+        #region EventHandlers
+        private void ShowError(string errorMessage)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(ShowError), new object[] { errorMessage });
+                return;
+            }
+
+            txt_right.Text += Environment.NewLine + errorMessage;
+        }
+
+        private void CheckValue(string value)
+        {
+            val = getID.Value;
+
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(CheckValue), new object[] { value });
+                return;
+            }
+
+            if (val.Length == 5)
+            {
+                string first    = val.Remove(1, 4);
+                string second   = val.Substring(1);
+
+                matchesFirst    = Regex.Matches(first, @"[A-Z]").Count;
+                matchesSecond   = Regex.Matches(second, @"[0-9]").Count;
+
+                if (matchesFirst == 1 && matchesSecond == 4)
+                {
+                    txt_left.Text +=  val + " MEGFELELŐ" + Environment.NewLine;
+                }
+                else
+                {
+                    txt_left.Text += val + " NEM MEGFELELŐ" + Environment.NewLine;
+                }
+                
+            }
+            else
+            {
+                txt_left.Text += val + " NEM MEGFELELŐ" + Environment.NewLine;
+            }
+        }
+        #endregion
+
+
+        #region ButtonClickEvents
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            getID.Go();
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            getID.Stop();
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            logname = "log-" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".txt";
+            using (StreamWriter objWriter = new StreamWriter(logname))
+            {
+                objWriter.Write(txt_left.Text);
+                MessageBox.Show("Log fájl mentése megtörtént: " + logname);
+            }
+        }
+        #endregion
+    }
+}
